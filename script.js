ca9624d8eb796d6053e80c7fec9431a2c996023b
dev-ahmedali <ahmedali69325@gmail.com>
2022-08-19 12:23:48 +0600

    added counter

--- script.js
+++ script.js
@@ -1,110 +1,171 @@
-// Select dom element
-const counterEl = document.getElementById('counter');
-const incrementEl = document.getElementById('increment');
-const decrementEl = document.getElementById('decrement');
-const resetEl = document.getElementById('reset');
-const addCounterEl = document.getElementById('addCounter');
-
-// added counter
-function addCount() {
-  const addCountReset = document.querySelector('.addcount-reset');
-  const counter = `<div class="mx-auto max-w-md mt-10 space-y-5">
-    <div
-      class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-    >
-      <div class="text-2xl font-semibold">0</div>
-      <div class="flex space-x-3">
-        <button
-          class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-        >
-          Increment
-        </button>
-        <button
-          class="bg-red-400 text-white px-3 py-2 rounded shadow"
-        >
-          Decrement
-        </button>
-      </div>
-    </div>
-  </div>`;
-  addCountReset.insertAdjacentHTML('afterend', counter);
-}
-
-// actions identifire
-const INCREMENT = 'increment';
-const DECREMENT = 'decrement';
-const RESET = 'reset';
-
-// action creators
-const increment = (value) => {
-  return {
-    type: INCREMENT,
-    payload: value,
-  };
-};
-
-const decrement = (value) => {
-  return {
-    type: DECREMENT,
-    payload: value,
-  };
-};
-const reset = (value) => {
-  return {
-    type: RESET,
-    payload: value,
-  };
-};
+let counterElement = document.getElementById('counter');
+const addCounterElement = document.getElementById('addCounter');
+const resetElement = document.getElementById('reset');
 
 // initial state
-const initialState = [
+let initialState = [
   {
     id: 1,
-    value: 0
-  }
-]
+    value: 0,
+  },
+];
 
-// added reducer function
-const counterReducer = (state = initialState, action) => {
-  if (action.type === INCREMENT) {
-    return {
-      ...state,
-      value: state.value + action.payload,
-    };
-  } else if (action.type === DECREMENT) {
-    return {
-      ...state,
-      value: state.value - action.payload,
-    };
-  } else if (action.type === RESET) {
-    return {
-      ...state,
+// create id
+let id = 1;
+
+// action identifiers
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+const ADDCOUNT = 'add';
+const RESET = 'reset';
+
+// create reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === ADDCOUNT) {
+    const updateState = state.map((item) => {
+      return {
+        ...item,
+      };
+    });
+    updateState.push({
+      id: initialState.length + id,
       value: 0,
-    };
+    });
+    ++id;
+    return updateState;
+  }
+  if (action.type === INCREMENT) {
+    const updateState = state.map((item) => {
+      return {
+        ...item,
+      };
+    });
+
+    const index = updateState.findIndex(
+      (item) => item.id === action.payload.id
+    );
+
+    updateState[index].value += action.payload.value;
+    return updateState;
+  } else if (action.type === DECREMENT) {
+    const updateState = state.map((item) => {
+      return {
+        ...item,
+      };
+    });
+    const index = updateState.findIndex(
+      (item) => item.id === action.payload.id
+    );
+    if (updateState[index].value > 0) {
+      updateState[index].value -= action.payload.value;
+    }
+
+    return updateState;
+  } else if (action.type === RESET) {
+    const updateState = state.map((item) => {
+      return {
+        ...item,
+      };
+    });
+    updateState.forEach((item) => (item.value = 0));
+    return updateState;
   } else {
     return state;
   }
+}
+
+// create store
+const store = Redux.createStore(counterReducer, initialState);
+
+// action creators
+const increment = (id, value) => {
+  return {
+    type: INCREMENT,
+    payload: {
+      value: value,
+      id: id,
+    },
+  };
 };
 
-// create Store
-const store = Redux.createStore(counterReducer);
+const decrement = (id, value) => {
+  return {
+    type: DECREMENT,
+    payload: {
+      value: value,
+      id: id,
+    },
+  };
+};
 
-const render = () => {
+const add = () => {
+  return {
+    type: ADDCOUNT,
+  };
+};
+
+const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+// add counter
+function createCounter(id, value) {
+  const div = document.createElement('div');
+  div.classList =
+    'p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow counter';
+  const counter = document.createElement('div');
+  counter.classList = 'text-2xl font-semibold';
+  div.appendChild(counter);
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList = 'flex space-x-3';
+  const incrementButton = document.createElement('button');
+  incrementButton.classList =
+    'bg-indigo-400 text-white px-3 py-2 rounded shadow';
+  incrementButton.innerText = 'Increment';
+  incrementButton.onclick = function () {
+    store.dispatch(increment(id, value));
+  };
+  const decrementButton = document.createElement('button');
+  decrementButton.classList = 'bg-red-400 text-white px-3 py-2 rounded shadow';
+  decrementButton.innerText = 'Decrement';
+  decrementButton.onclick = function () {
+    store.dispatch(decrement(id, value));
+  };
+  buttonContainer.appendChild(incrementButton);
+  buttonContainer.appendChild(decrementButton);
+  div.appendChild(buttonContainer);
+  counterElement.append(div);
+}
+
+createCounter(1, 1);
+
+addCounterElement.addEventListener('click', () => {
+  let id = store.getState().length + 1;
+  let value = prompt(
+    'Enter how many numbers you want to increment or decrement on each click: '
+  );
+  value = parseInt(value);
+  createCounter(id, value);
+  store.dispatch(add());
+});
+
+resetElement.addEventListener('click', () => {
+  const isReset = confirm('Are you sure you want Reset all counter value?');
+  if (isReset) {
+    store.dispatch(reset());
+  }
+});
+
+// create render function
+function render() {
   const state = store.getState();
-  counterEl.innerText = state.value.toString();
-};
+  state.forEach((item, index) => {
+    counterElement.children[index].children[0].innerText = item.value;
+  });
+}
 
 render();
+
 store.subscribe(render);
-
-incrementEl.addEventListener('click', () => {
-  store.dispatch(increment(1));
-});
-
-decrementEl.addEventListener('click', () => {
-  store.dispatch(decrement(1));
-});
-
-resetEl.addEventListener('click', () => {
-  store.dispatch(reset(0));
-});
